7953291f3d5c3a00dc2ba087df3bd280b8567dde
c-ishibashi <c-ishibashi@race-ss.co.jp>
2025-03-27 13:57:32 +0900

    【修正】タスク一覧
    web.phpルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
 
-Route::get('/api/tasks', [TaskController::class, 'index']);
 Route::get('/{any}', function() {
     return view('app');
 })->where('any', '.*');

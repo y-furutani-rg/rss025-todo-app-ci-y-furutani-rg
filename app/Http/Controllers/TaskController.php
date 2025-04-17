6e921c9dbcba8476d37757a2f361b159e0172bd4
c-ishibashi <c-ishibashi@race-ss.co.jp>
2025-04-17 13:26:01 +0900

    【修正】タスク追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
@@ -12,13 +13,14 @@ class TaskController extends Controller
      *
      * @return JsonResponse
      */
-    public function index() : JsonResponse
+    public function index(): JsonResponse
     {
         try {
             $tasks = Task::all();
             return response()->json($tasks);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
+            return response()->json(['error' => 'Internal Server Error'], 500);
         }
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array<string>
+     */
     protected $fillable = [
         'title',
         'content',

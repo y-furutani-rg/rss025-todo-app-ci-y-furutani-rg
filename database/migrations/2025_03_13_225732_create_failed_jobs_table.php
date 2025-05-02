9b4f5ac0944d23ee02f6e9c8038cfc7544ab93d9
執行 洋平 <shigyo@race-ss.co.jp>
2025-05-02 18:31:43 +0900

    Revert "Revert "Merge branch 'fixed/migrate' into 'master'""
    This reverts commit 4d0eddfa99231a70f1c2a2cade63231ddf5ab3dd

--- database/migrations/2025_03_13_225732_create_failed_jobs_table.php
+++ database/migrations/2025_03_13_225732_create_failed_jobs_table.php
@@ -11,14 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('cache', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumText('value');
+            $table->integer('expiration');
+        });
+
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+
         });
     }
 
@@ -27,6 +30,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
     }
 };

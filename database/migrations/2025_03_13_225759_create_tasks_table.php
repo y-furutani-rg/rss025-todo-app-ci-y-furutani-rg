a8431b83009c914f99c17d94865987e6f588cc35
sa-kawano <sa-kawano@race-ss.co.jp>
2025-06-09 18:58:54 +0900

    追加：マイグレーションファイル修正

--- database/migrations/2025_03_13_225759_create_tasks_table.php
+++ database/migrations/2025_03_13_225759_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->string('person_in_charge');
             $table->timestamps();
             $table->softDeletes();

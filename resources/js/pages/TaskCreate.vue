73233fb4503816801892108be3add022a98a5da1
Furutani0731 <y-furutani@race-ss.co.jp>
2025-07-22 19:08:29 +0900

    RSS025_TRAINING_PJ-59Vue.js＋LaravelのTODOリスト：登録画面作成_3次CR1回目修正

--- resources/js/pages/TaskCreate.vue
+++ resources/js/pages/TaskCreate.vue
@@ -2,16 +2,16 @@
     <form>
         <div class="w-80 mx-auto pt-4">
             <div class="mb-4">
-                <label class="text-sm text-gray-500 font-medium">タイトル</label>
-                <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
+                <label for="title" class="text-sm text-gray-500 font-medium">タイトル</label>
+                <input id="title" type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
             </div>
             <div class="mb-4">
-                <label class="text-sm text-gray-500 font-medium">内容</label>
-                <textarea class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md"></textarea>        
+                <label for="content" class="text-sm text-gray-500 font-medium">内容</label>
+                <textarea id="content" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md"></textarea>        
             </div>
             <div class="mb-4">
-                <label class="text-sm text-gray-500 font-medium">担当者</label>
-                <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
+                <label for="person_in_charge" class="text-sm text-gray-500 font-medium">担当者</label>
+                <input id="person_in_charge" type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
             </div>
             <button type="submit" class="w-full p-[10px] bg-[var(--color-accent)] text-white text-center border-1 cursor-pointer rounded-md">追加</button>
         </div>

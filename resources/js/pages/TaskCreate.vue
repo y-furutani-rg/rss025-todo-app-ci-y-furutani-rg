75ec6298378e62689af6df020409c140b428c9e5
Furutani0731 <y-furutani@race-ss.co.jp>
2025-07-22 16:01:37 +0900

    RSS025_TRAINING_PJ-59Vue.js＋LaravelのTODOリスト：登録画面作成_2次CR1回目修正

--- resources/js/pages/TaskCreate.vue
+++ resources/js/pages/TaskCreate.vue
@@ -2,15 +2,15 @@
     <form>
         <div class="w-80 mx-auto pt-4">
             <div class="mb-4">
-                <h1 class="text-sm text-gray-500 font-medium">タイトル</h1>
+                <label class="text-sm text-gray-500 font-medium">タイトル</label>
                 <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
             </div>
             <div class="mb-4">
-                <h1 class="text-sm text-gray-500 font-medium">内容</h1>
+                <label class="text-sm text-gray-500 font-medium">内容</label>
                 <textarea class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md"></textarea>        
             </div>
             <div class="mb-4">
-                <h1 class="text-sm text-gray-500 font-medium">担当者</h1>
+                <label class="text-sm text-gray-500 font-medium">担当者</label>
                 <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
             </div>
             <button type="submit" class="w-full p-[10px] bg-[var(--color-accent)] text-white text-center border-1 cursor-pointer rounded-md">追加</button>

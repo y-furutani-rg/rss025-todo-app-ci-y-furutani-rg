1ee92b42f772a50e722a384269ca411732b7724b
Furutani0731 <y-furutani@race-ss.co.jp>
2025-07-22 15:25:13 +0900

    RSS025_TRAINING_PJ-59Vue.js＋LaravelのTODOリスト：登録画面作成

---
+++ resources/js/pages/TaskCreate.vue
@@ -0,0 +1,19 @@
+<template>
+    <form>
+        <div class="w-80 mx-auto pt-4">
+            <div class="mb-4">
+                <h1 class="text-sm text-gray-500 font-medium">タイトル</h1>
+                <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
+            </div>
+            <div class="mb-4">
+                <h1 class="text-sm text-gray-500 font-medium">内容</h1>
+                <textarea class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md"></textarea>        
+            </div>
+            <div class="mb-4">
+                <h1 class="text-sm text-gray-500 font-medium">担当者</h1>
+                <input type="text" class="w-full py-2 px-4 border-1 focus:outline-none focus:border-blue-500 cursor-pointer rounded-md">
+            </div>
+            <button type="submit" class="w-full p-[10px] bg-[var(--color-accent)] text-white text-center border-1 cursor-pointer rounded-md">追加</button>
+        </div>
+    </form>
+</template>
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TaskList from './pages/TaskList.vue';
+import TaskCreate from './pages/TaskCreate.vue';
 
 const routes = [
     {
         path: '/tasks', // ルートパスにアクセスしたときに
         component: TaskList, // TaskList コンポーネントを表示する
+    },
+    {
+        path: '/tasks/create',
+        component: TaskCreate,
     }
 ];
 

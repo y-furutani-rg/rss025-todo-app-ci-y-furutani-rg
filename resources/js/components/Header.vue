0f01c6471dcbce3fd7a6f58eb4369e6a0ae6c3af
Furutani0731 <y-furutani@race-ss.co.jp>
2025-07-29 16:37:52 +0900

    RSS025_TRAINING_PJ-61 Vue.js＋LaravelのTODOリスト：ヘッダー作成

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from './Button.vue';
+</script>
+
+<template>
+    <header class="flex">
+        <Button link="http://localhost/tasks" name="タスク一覧" />
+        <Button link="http://localhost/task/create" name="タスク追加" />
+    </header>
+</template>

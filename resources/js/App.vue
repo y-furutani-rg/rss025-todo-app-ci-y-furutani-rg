53a46c8fd7de2f9a0378a3c19a806715c9622e6e
Furutani0731 <y-furutani@race-ss.co.jp>
2025-07-29 16:37:52 +0900

    RSS025_TRAINING_PJ-270 Vue.js＋LaravelのTODOリスト：ヘッダー作成

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div id="app-container">
+        <Header></Header>
         <FlashMessage :key="$route.fullPath" />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 import FlashMessage from './components/FlashMessage.vue';
 export default {
     name: 'App',
     components: {
+        Header,
         FlashMessage
     }
 };

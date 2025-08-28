6e9f87f2c4b7497278cc911579653da858a72a5b
Furutani0731 <y-furutani@race-ss.co.jp>
2025-08-28 13:18:37 +0900

    Add PHPUnit test and run on CI

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+// tests/ExampleTest.php の内容
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
